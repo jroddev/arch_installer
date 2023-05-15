bd9c0d9bb0b5dedc31912224948b18699fe1c43f
jroddev <jroddev@gmail.com>
2023-05-15 11:38:04 +1000

    try with parted -s

--- encrypted_partition.sh
+++ encrypted_partition.sh
@@ -2,18 +2,22 @@
 
 # Identify the correct device from your system with `lsblk`
 DISK="/dev/nvme0n1"
-ESP_SIZE="512M"
+ESP_SIZE="512MB"
 CRYPT_NAME="cryptroot"
 PBKDF2_ITERATIONS=10000
 
 # Create ESP partition
 # ef00 is the partition type guide reserved for ESP
-echo "Creating the EFI System Partition (ESP)
-sgdisk -n 1:0:+ESP_SIZE -t 1:ef00 -c 1:"EFI System Partition" $DISK
+echo "Creating the EFI System Partition (ESP)"
+# sgdisk -n 1:0:+ESP_SIZE -t 1:ef00 -c 1:"EFI System Partition" $DISK
+parted -s $DISK mklabel gpt
+parted -s $DISK mkpart primary fat32 1MiB $ESP_SIZE
+parted -s $DISK set 1 esp on
 
 # Create root partition
 echo "Creating the Linux Root Partition"
-sgdisk -n 2:0:0 -t 2:8300 -c 2:"Arch Linux Root" $DISK
+# sgdisk -n 2:0:0 -t 2:8300 -c 2:"Arch Linux Root" $DISK
+parted -s $DISK mkpart primary ext $ESP_SIZE 100%
 
 # Prompt for LUKs passphrase
 echo "Enter a passphrase for LUKS container:"
@@ -26,10 +30,10 @@ echo $PASSPHRASE | cryptsetup open --batch-mode ${DISK}p2 $CRYPT_NAME
 
 echo "Format Crypt Partition with ext4"
 mkfs.ext4 /dev/mapper/$CRYPT_NAME
-ehco "Mount Linux Root to /mnt"
+echo "Mount Linux Root to /mnt"
 mount dev/mapper/cryptroot /mnt
 
-echo "Format ${DISK}p1 EPS with fat32
+echo "Format ${DISK}p1 EPS with fat32"
 mkdir -p /mnt/boot
 mkfs.fat -F32 ${DISK}p1
 mount ${DISK}p1 /mnt/boot
