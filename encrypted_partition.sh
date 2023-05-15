e63dd9823208e8fa6f9c0d5a0ad74ce530e15a97
jroddev <jroddev@gmail.com>
2023-05-15 11:47:01 +1000

    Fix a couple of typos

--- encrypted_partition.sh
+++ encrypted_partition.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 # Identify the correct device from your system with `lsblk`
 DISK="/dev/nvme0n1"
@@ -6,20 +7,14 @@ ESP_SIZE="512MB"
 CRYPT_NAME="cryptroot"
 PBKDF2_ITERATIONS=10000
 
-# Create ESP partition
-# ef00 is the partition type guide reserved for ESP
 echo "Creating the EFI System Partition (ESP)"
-# sgdisk -n 1:0:+ESP_SIZE -t 1:ef00 -c 1:"EFI System Partition" $DISK
 parted -s $DISK mklabel gpt
 parted -s $DISK mkpart primary fat32 1MiB $ESP_SIZE
 parted -s $DISK set 1 esp on
 
-# Create root partition
 echo "Creating the Linux Root Partition"
-# sgdisk -n 2:0:0 -t 2:8300 -c 2:"Arch Linux Root" $DISK
-parted -s $DISK mkpart primary ext $ESP_SIZE 100%
+parted -s $DISK mkpart primary ext4 $ESP_SIZE 100%
 
-# Prompt for LUKs passphrase
 echo "Enter a passphrase for LUKS container:"
 read -s PASSPHRASE
 
